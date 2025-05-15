d0691931cc1492dcebf5458a8c0df458926d8ce7
Marcin <skorekmarcin@gmail.com>
2025-05-15 19:38:03 +0200

    chore: cleanup migrations

--- supabase/migrations/20250515173619_initial_schema.sql
+++ supabase/migrations/20250515173619_initial_schema.sql
@@ -1,3 +1,45 @@
+-- wishlists table
+create table if not exists "wishlists" (
+  id serial primary key,
+  uuid uuid not null default gen_random_uuid() unique,
+  name varchar(100) not null,
+  description varchar(250),
+  created_at timestamp with time zone default now() not null,
+  updated_at timestamp with time zone default now() not null,
+  author_id uuid references auth.users(id) not null
+);
+
+-- Add row-level security policies
+alter table public.wishlists enable row level security;
+
+-- Create policies
+create policy "Users who know the wishlist uuid can view the wishlist"
+  on public.wishlists
+  for select
+  using (true);
+
+create policy "Users can insert new wishlists when authenticated"
+  on public.wishlists
+  for insert
+  to authenticated
+  with check (true);
+
+create policy "Users can only delete their own wishlists"
+  on public.wishlists
+  for delete
+  using ((select auth.uid()) = author_id);
+
+create policy "Users can update their own wishlists"
+  on public.wishlists
+  for update
+  using ((select auth.uid()) = author_id);
+
+-- Add index on author_id
+create index wishlists_author_id_idx on public.wishlists (author_id);
+
+-- Add index on uuid
+create index wishlists_uuid_idx on public.wishlists (uuid);
+
 -- enum priority (low, medium, high)
 create type priority as enum ('low', 'medium', 'high');
 
