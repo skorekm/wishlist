ceb2fcaaca2fa7f7a19da293f03de2e51bb7421a
Marcin <skorekmarcin@gmail.com>
2025-05-22 10:32:54 +0200

    feat: add tests for adding a new list

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
+/// <reference types="vitest/config" />
+
 import { defineConfig } from 'vite'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
@@ -15,5 +16,9 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
   },
 })
