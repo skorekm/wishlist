db5eff32ecc8e8acca624f276dd6896881f058b3
Marcin <skorekmarcin@gmail.com>
2025-10-24 15:11:19 +0200

    chore: update tests pt 2344322

--- vite.config.ts
+++ vite.config.ts
@@ -17,9 +17,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./vitest.setup.ts'],
-  },
 })
