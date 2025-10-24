e51cf9ca86549ff2fb5e672a7966f7352421f766
Marcin <skorekmarcin@gmail.com>
2025-10-24 14:56:39 +0200

    feat: move to bun

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 import { Database } from '@/database.types'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = import.meta.env.SUPABASE_URL
+const supabaseAnonKey = import.meta.env.SUPABASE_ANON_KEY
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Missing Supabase environment variables. Please check your .env file.')
